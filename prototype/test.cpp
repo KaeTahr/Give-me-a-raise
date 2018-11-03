b8f8e0a2c54a93231215f54853124b82c5a1212e
kaetahr <lostech@tuta.io>
2018-11-02 20:03:39 -0600

    inital commit

---
+++ prototype/test.cpp
@@ -0,0 +1,55 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+//Global Variables
+bool bGameRunning;
+
+string getUserInput()
+{ 
+	string sInput;
+	cout << "What will you do?" << endl;
+	cin >> sInput;
+	
+	return sInput;
+}
+
+void parseInput(string sInput)
+{ 
+	if (sInput == "dab")
+	{ 
+		 cout << "You dab" << endl << "*dabs" << endl;
+	}
+	if ( sInput == "attack" || sInput == "hit" || sInput == "strike")
+	{ 
+		cout << "You attack women and minorities" << endl;
+	}
+	if ( sInput == "quit")
+	{
+		bGameRunning = 0;
+	}
+}
+
+int main ()
+{ 
+	bGameRunning = 1;
+
+	while ( bGameRunning == 1)
+	{ 
+	
+		//Variables and Strings
+		string sUserInput;
+
+		//Input
+		sUserInput = getUserInput();
+
+		//Processing
+		parseInput(sUserInput);
+
+		//Ouput
+		//printStory();
+	}
+	
+	return 0;
+
+}
